c4eddf6318233f70c717d32be774957f0a0dd4a0
Silas <roessle.bsn@gmail.com>
2026-06-01 19:07:45 +0200

    fix: force-dynamic to prevent DOMMatrix prerender error

--- app/lieferung/lieferschein/page.tsx
+++ app/lieferung/lieferschein/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from "react";
 import AuthGuard from "../../components/AuthGuard";
 import ProgressBar from "../../components/ProgressBar";
